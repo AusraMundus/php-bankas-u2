f34da72040256763c7bedf9630ccb4f440cba3ce
Ausra <munduswebstudio@gmail.com>
2023-06-08 10:40:52 +0300

    Add. Delete.

---
+++ bankas/public/add-account.php
@@ -0,0 +1,74 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+$accounts = file_get_contents(__DIR__ . '/../accounts.ser');
+$accounts = $accounts ? unserialize($accounts) : [];
+
+$accounts[] = [
+    'firstName' => $_POST['firstName'],
+    'lastName' => $_POST['lastName'],
+    'personalId' => $_POST['personalId'],
+    'accountNo' => $_POST['accountNo'],
+    'id' => rand(100000000, 999999999)
+];
+$accounts = serialize($accounts);
+file_put_contents(__DIR__ . '/../accounts.ser', $accounts);
+header('Location: ./');
+die;
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <script src="app.js"></script>
+    <title>Pridėti sąskaitą</title>
+</head>
+
+<body>
+
+    <?php require __DIR__ . '/menu.php' ?>
+
+    <div class="card">
+        <h5 class="card-header">Pridėkite naują sąskaitą</h5>
+        <div class="card-body">
+            
+            <form class="row g-3" form action="./add-account.php" method="post">
+                <div class="col-md-6">
+                    <label for="firstName" class="form-label">Vardas</label>
+                    <input type="text" class="form-control" name="firstName">
+                </div>
+                <div class="col-md-6">
+                <label for="lastName" class="form-label">Pavardė</label>
+                    <input type="text" class="form-control" name="lastName">
+                </div>
+                <div class="col-12">
+                    <label for="personalId" class="form-label">Asmens kodas</label>
+                    <input type="text" class="form-control" name="personalId" placeholder="A. K.">
+                </div>
+                <div class="col-12">
+                    <label for="accountNo" class="form-label">Banko sąskaitos numeris</label>
+                    <input type="text" class="form-control" name="accountNo" placeholder="LT...">
+                </div>
+                
+                <div class="col-12">
+                    <button type="submit" class="btn btn-success">Pridėti sąskaitą</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
--- bankas/public/index.php
+++ bankas/public/index.php
@@ -1,3 +1,10 @@
+<?php
+
+$accounts = file_get_contents(__DIR__ . '/../accounts.ser');
+$accounts = $accounts ? unserialize($accounts) : [];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,7 +21,47 @@
 
     <?php require __DIR__ . '/menu.php' ?>
 
-    
+    <div class="card">
+        <h5 class="card-header">Banko sąskaitos</h5>
+        <div class="card-body">
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Vardas</th>
+                        <th scope="col">Pavardė</th>
+                        <th scope="col">Asmens kodas</th>
+                        <th scope="col">Sąskaitos Nr.</th>
+                        <th scope="col">Balansas</th>
+                        <th colspan="2">Disponavimas lėšomis</th>
+                        <th scope="col">Sąskaitos panaikinimas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($accounts as $account) : ?>
+                        <tr>
+                            <td><?= $account['firstName'] ?></td>
+                            <td><?= $account['lastName'] ?></td>
+                            <td><?= $account['personalId'] ?></td>
+                            <td><?= $account['accountNo'] ?></td>
+                            <td>0 Eur</td>
+                            <td><button class="btn btn-success">PRIDĖTI</button></td>
+                            <td><button class="btn btn-warning">IŠIMTI</button></td>
+                            <td>
+                                <form action="./delete-account.php?id=<?= $account['id'] ?>" method="post">
+                                    <button type="submit" class="btn btn-danger">IŠTRINTI</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+
+
 </body>
 
 </html>
